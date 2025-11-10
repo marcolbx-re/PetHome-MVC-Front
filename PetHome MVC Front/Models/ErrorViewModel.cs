37bfc6642702132873063f39ba88e26f3d523b93
marcolbx <239292571+marcolbx-re@users.noreply.github.com>
2025-11-10 18:21:10 +0100

    Pethome front index

---
+++ PetHome MVC Front/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetHome_MVC_Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
